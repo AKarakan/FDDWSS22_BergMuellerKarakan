fade29933f0356bfe4162a40ea215f233f314ecf
Abdurrahman Karakan <abdu.karakan@googlemail.com>
2022-09-01 15:09:52 +0200

    chat mit spiel Server verbunden

--- Chat/index.js
+++ Chat/index.js
@@ -4,19 +4,38 @@ const http = require('http');
 const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
+//const axios = require("axios")
 
 app.get("/", (req,res) =>{
     res.sendFile(__dirname+"/files/index.html");
 })
 
-let befehle = []
+let getBefehle = ["/aktStand"]
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
     socket.on('chat message', (msg) => {
-        if(msg === "hallo") {
-            console.log("hier kommt dann der befehlt für den request in das spiel")
-        }
+        if(getBefehle.some((elem)=> elem == msg )) {
+            console.log("incomming Befehlt: "+ msg)
+            const options = {
+                hostname: 'localhost',
+                port: 3000,
+                path: msg,
+                method: 'GET',
+              };
+              const req = http.request(options, res => {
+                retMsg = res.on('data', d => {
+                    socket.emit('game event', JSON.parse(d.toString()).message)
+                });
+              });
+              
+              req.on('error', error => {
+                console.error(error);
+              });
+              
+              req.end();
+            }
         else{
             console.log('message: ' + msg);
             io.emit('chat message', msg);
@@ -32,5 +51,5 @@ io.on('connection', (socket) => {
     })
 });
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3001;
 server.listen(port,()=> console.log("server listening on port:" + port))
