721d50c986913e64f6e97f8c544d2109a172e4ad
Abdurrahman Karakan <abdu.karakan@googlemail.com>
2022-09-02 18:59:51 +0200

    volle verbindung zum game Server hergestellt

--- Chat/index.js
+++ Chat/index.js
@@ -10,42 +10,48 @@ app.get("/", (req,res) =>{
     res.sendFile(__dirname+"/files/index.html");
 })
 
-let getBefehle = ["/aktStand"]
+let getBefehle = ["/aktStand","/aktPunkte"]
+let postBefehle = ["/start","/wuerfeln"]
+let postParamBefehle = [ "/behaupten","/challenge"]
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
     socket.on('chat message', (msg) => {
-        if(getBefehle.some((elem)=> elem == msg )) {
-            console.log("incomming Befehlt: "+ msg)
-            const options = {
-                hostname: 'localhost',
-                port: 3000,
-                path: msg,
-                method: 'GET',
-              };
-              const req = http.request(options, res => {
-                retMsg = res.on('data', d => {
-                    socket.emit('game event', JSON.parse(d.toString()).message)
-                });
-              });
-              
-              req.on('error', error => {
-                console.error(error);
-              });
-              
-              req.end();
+
+        path = msg.split(" ")[0]
+        param = msg.split(" ")[1]
+
+        if(getBefehle.some((elem)=> elem == path )) {
+            console.log("incomming get Befehlt: "+ path)
+            makeGetRequest(path)
+            .then((res)=> socket.emit('game event', JSON.parse(res).message))
+            .catch((err)=>socket.emit('game event', err))
+            
             }
+        else if(postBefehle.some((elem)=> elem == path)){
+            console.log("incomming post Befehlt: "+ path)
+            makePostRequest(path)
+            .then((res)=> socket.emit('game event', JSON.parse(res).message))
+            .catch((err)=>socket.emit('game event', err))
+
+        }
+        else if(postParamBefehle.some((elem)=> elem == path)){
+            console.log("incomming post param Befehlt: "+ path)
+            makePostRequestWithParam(path,param)
+            .then((res)=> {
+                console.log(res)
+                io.emit('game event', JSON.parse(res).message)
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+        }
         else{
             console.log('message: ' + msg);
             io.emit('chat message', msg);
         }
     });
-
-    socket.on('gameEvent', (msg) => {
-        console.log(msg)
-    })
-
     socket.on('disconnect', ()=>{
         console.log("User disconnected");
     })
@@ -53,3 +59,76 @@ io.on('connection', (socket) => {
 
 let port = process.env.PORT || 3001;
 server.listen(port,()=> console.log("server listening on port:" + port))
+
+function makeGetRequest(path) {  
+    return new Promise((resolve, reject) =>{
+        const options = {
+            hostname: 'localhost',
+            port: 3000,
+            path: path,
+            method: 'GET',
+        };
+        const req = http.request(options, res => {
+            retMsg = res.on('data', d => {
+                resolve(d.toString())
+            });
+        });
+        req.on('error', error => {
+            console.error(error);
+            reject(error)
+        });
+        
+        req.end();
+    })
+};
+
+function makePostRequest(path){
+    return new Promise((resolve, reject) =>{
+        const options = {
+            hostname: 'localhost',
+            port: 3000,
+            path: path,
+            method: 'POST',
+        };
+        const req = http.request(options, res => {
+            retMsg = res.on('data', d => {
+                resolve(d.toString())
+            });
+        });
+        req.on('error', error => {
+            console.error(error);
+            reject(error)
+        });
+        
+        req.end();
+    })
+}
+
+function makePostRequestWithParam(path,param){
+    return new Promise((resolve, reject) =>{
+        postData = JSON.stringify({
+            behauptung: param
+        })
+        const options = {
+            hostname: 'localhost',
+            port: 3000,
+            path: path,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': postData.length
+            }
+        };
+        const req = http.request(options, res => {
+            retMsg = res.on('data', d => {
+                resolve(d.toString())
+            });
+        });
+        req.on('error', error => {
+            console.error(error);
+            reject(error)
+        });
+        req.write(postData)
+        req.end();
+    })
+}
